913dc4324b5e622b70e11ee72ed36fe213eba7d0
사용자 <sayongja@sayongjas-MacBook-Air.local>
2021-07-12 03:04:36 +0900

    ADD: Axios로 유기동물API data 받아옴

---
+++ src/service/get-data.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export async function getCat() {
+	try {
+		const {
+			data: {
+				data: {
+					response: {
+						body: {
+							items: { item },
+						},
+					},
+				},
+			},
+		} = await axios({
+			url: 'https://cors-proxy.org/api/',
+			method: 'get',
+			headers: {
+				'cors-proxy-url':
+					'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?upr_cd=6110000&upkind=422400&numOfRows=50&ServiceKey=Rs6VFrkuCiM5A3qb%2Fh%2F%2BIe0ieawIWS7FA6yQygtC7%2FaMzfKCAg9mK73TVPHQ76hD9ZSr2rwzR%2FNJ367Y7Lz%2Big%3D%3D',
+			},
+		});
+		return item;
+	} catch (error) {
+		console.log(error);
+	}
+}
