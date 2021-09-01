34d5b7fb5c91b6d581d498a6bf50eb98d315ee78
Songchangyeop <waitwait0301@naver.com>
2021-09-02 01:18:55 +0900

    ADD: Main페이지 footer 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Footer() {
-	return <Foot></Foot>;
+	return <Foot>© 2021 Friends - by changyeop Song</Foot>;
 }
 
 export default Footer;
