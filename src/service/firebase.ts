6bcd36e9ffcbe259780623a31b0152039bc524eb
Songchangyeop <waitwait0301@naver.com>
2021-10-12 00:53:15 +0900

    ADD: GoogleAuth 로그인 로그아웃 구현

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,9 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,5 +7,4 @@ const firebaseConfig = {
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const firebaseApp = initializeApp(firebaseConfig);
